740773953153e591e5ac70c97dc895b8424aacac
cornernote <cornernote@gmail.com>
2013-12-23 14:11:44 +1030

    update mysql_setup.sh

--- tests/travis/mysql_setup.sh
+++ tests/travis/mysql_setup.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-mysql -u root -e 'CREATE SCHEMA `email` CHARACTER SET utf8 COLLATE utf8_general_ci; GRANT ALL ON `email`.* TO test@localhost IDENTIFIED BY "test"; FLUSH PRIVILEGES;'
+mysql -e 'create database email;'
