25bbd065a7b1a20cff2167ad0948f24bfde90094
cornernote <cornernote@gmail.com>
2013-12-23 16:57:58 +1030

    update travis

--- tests/acceptance/WelcomeCept.php
+++ tests/acceptance/WelcomeCept.php
@@ -2,6 +2,4 @@
 $I = new WebGuy($scenario);
 $I->wantTo('ensure that frontpage works');
 $I->amOnPage('/'); 
-$I->see('Index of /');
-$I->click('test'); 
-$I->see('test2.csv');
+$I->see('It works!');
