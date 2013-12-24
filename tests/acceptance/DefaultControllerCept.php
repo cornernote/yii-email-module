cd9ac30deb333bc0f3589727982f04d5ccc9e7fd
cornernote <cornernote@gmail.com>
2013-12-24 14:43:36 +1030

    travis

--- tests/acceptance/DefaultControllerCept.php
+++ tests/acceptance/DefaultControllerCept.php
@@ -17,7 +17,7 @@ $I = new WebGuy($scenario);
 $I->wantTo('ensure DefaultController works');
 
 $I->amOnPage('email');
-$I->see('You may use the following tools to help manage email within your application.');
+$I->see('You may use the following tools');
 
 $I->click('Spool');
 $I->see('Spools');
