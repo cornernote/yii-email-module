e9ffcaf228540a21637970577b462f12e1faadc8
cornernote <cornernote@gmail.com>
2014-02-07 21:49:54 +1030

    test suite - update local webserver location

--- tests/_components/SiteController.php
+++ tests/_components/SiteController.php
@@ -7,9 +7,9 @@
  * @author Zain Ul abidin <zainengineer@gmail.com>
  * @copyright 2013 Mr PHP
  * @link https://github.com/cornernote/yii-audit-module
- * @license BSD-3-Clause https://raw.github.com/cornernote/yii-audit-module/master/LICENSE
+ * @license BSD-3-Clause https://raw.github.com/cornernote/yii-email-module/master/LICENSE
  *
- * @package yii-audit-module
+ * @package yii-email-module
  */
 class SiteController extends CController
 {
