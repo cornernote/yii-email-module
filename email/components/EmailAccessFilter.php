80cdd97dc0140cdad82e715c5b75aa25b53d9b96
cornernote <cornernote@gmail.com>
2016-01-04 14:01:55 +1030

    allow ipFilters

--- email/components/EmailAccessFilter.php
+++ email/components/EmailAccessFilter.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * EmailAccessFilter
  *
@@ -21,11 +22,48 @@ class EmailAccessFilter extends CFilter
     protected function preFilter($filterChain)
     {
         $app = Yii::app();
-        /** @var emailModule $email */
+        /** @var EmailModule $email */
         $email = $app->getModule('email');
-        if (!in_array($app->getUser()->getName(), $email->adminUsers))
+        if (!$this->allowUser($email, $app->getUser()) || !$this->allowIp($email, Yii::app()->request->userHostAddress)) {
             throw new CHttpException(403, 'You are not allowed to access this page.');
+        }
         return parent::preFilter($filterChain);
+    }
+
+    /**
+     * Checks to see if the user IP is allowed by {@link ipFilters}.
+     * @param $email EmailModule
+     * @param $user
+     * @return bool whether the user IP is allowed by <a href='psi_element://ipFilters'>ipFilters</a>.
+     * @throws CHttpException
+     * @internal param string $ip the user IP
+     */
+    protected function allowUser($email, $user)
+    {
+        if (empty($email->adminUsers)) {
+            return true;
+        }
+        if (in_array($user->getName(), $email->adminUsers)) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Checks to see if the user IP is allowed by {@link ipFilters}.
+     * @param $email EmailModule
+     * @param string $ip the user IP
+     * @return bool whether the user IP is allowed by <a href='psi_element://ipFilters'>ipFilters</a>.
+     */
+    protected function allowIp($email, $ip)
+    {
+        if (empty($email->ipFilters))
+            return true;
+        foreach ($email->ipFilters as $filter) {
+            if ($filter === '*' || $filter === $ip || (($pos = strpos($filter, '*')) !== false && !strncmp($ip, $filter, $pos)))
+                return true;
+        }
+        return false;
     }
 
 }
